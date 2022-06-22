0c1e0da06bde2780c31628bb5737e5a89fba6de0
shinx <626361893@qq.com>
2022-06-22 23:09:41 +0800

    update bp-02-001

---
+++ testnet/tasks/BP-02-001/test.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+#-----------------------------
+
+#解析JSON
+function getJsonValuesByAwk() {
+    awk -v json="$1" -v key="$2" -v defaultValue="$3" 'BEGIN{
+        foundKeyCount = 0
+        while (length(json) > 0) {
+            # pos = index(json, "\""key"\""); ## 这行更快一些，但是如果有value是字符串，且刚好与要查找的key相同，会被误认为是key而导致值获取错误
+            pos = match(json, "\""key"\"[ \\t]*?:[ \\t]*");
+            if (pos == 0) {if (foundKeyCount == 0) {print defaultValue;} exit 0;}
+
+            ++foundKeyCount;
+            start = 0; stop = 0; layer = 0;
+            for (i = pos + length(key) + 1; i <= length(json); ++i) {
+                lastChar = substr(json, i - 1, 1)
+                currChar = substr(json, i, 1)
+
+                if (start <= 0) {
+                    if (lastChar == ":") {
+                        start = currChar == " " ? i + 1: i;
+                        if (currChar == "{" || currChar == "[") {
+                            layer = 1;
+                        }
+                    }
+                } else {
+                    if (currChar == "{" || currChar == "[") {
+                        ++layer;
+                    }
+                    if (currChar == "}" || currChar == "]") {
+                        --layer;
+                    }
+                    if ((currChar == "," || currChar == "}" || currChar == "]") && layer <= 0) {
+                        stop = currChar == "," ? i : i + 1 + layer;
+                        break;
+                    }
+                }
+            }
+
+            if (start <= 0 || stop <= 0 || start > length(json) || stop > length(json) || start >= stop) {
+                if (foundKeyCount == 0) {print defaultValue;} exit 0;
+            } else {
+                print substr(json, start, stop - start);
+            }
+
+            json = substr(json, stop + 1, length(json) - stop)
+        }
+    }'
+}
+
+#HTTP RPC接口请求  method klay_syncing 当节点同步时返回结果正常，当节点不在同步时，返回的result = false
+json=$(curl -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"klay_syncing","params":[],"id":1}' http://134.119.192.13:8551)
+#解析json中的result字段,如果不正常了则删除false
+RESULT=getJsonValuesByAwk "$json" "result" "defaultValue"
+if [ $RESULT ]
+then
+    echo success 
+else 
+    #之后可以通过邮箱发送节点挂掉的通知
+    echo false
+fi
